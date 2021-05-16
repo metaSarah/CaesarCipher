0a04c87eb90edb5eb0fd169a423d6b9b39c18ca6
metaSarah <84154961+metaSarah@users.noreply.github.com>
2021-05-16 15:23:06 -0400

    Create caesar.py
    Init commit

---
+++ caesar.py
@@ -0,0 +1,9 @@
+def encrypt(key,plaintext):
+    ciphertext=""
+    #YOUR CODE HERE
+    return ciphertext
+
+def decrypt(key,ciphertext):
+    plaintext=""
+    #YOUR CODE HERE
+    return plaintext
